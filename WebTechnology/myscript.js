b27727c984cba31b50045e95bc65e9db862e1903
unknown <technowings.india@gmail.com>
2021-03-12 08:56:10 +0530

    update

---
+++ WebTechnology/myscript.js
@@ -0,0 +1,10 @@
+function text1()
+    {
+        document.getElementById("script1").innerHTML="This text written inside javascript external file"
+    }
+
+
+    function mousefun()
+    {
+        document.write("<h4> hello mouse over </h4>");
+    }
